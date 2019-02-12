3c5764a8895646efe24cf9470545e5010d3185e2
a.aitken <a.aitken@statdata.kz>
2019-02-12 11:43:28 +0600

    6

--- src/main/java/stream/ParallelStreams.java
+++ src/main/java/stream/ParallelStreams.java
@@ -1,5 +1,6 @@
 package stream;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,5 +35,30 @@ public class ParallelStreams {
                 .collect(Collectors.groupingByConcurrent(String::length));
 
         System.out.println(result.get(1));
+
+
+
+        //example with Object
+        User user1 = new User(1L,"ccc1",25,"ccc1@bk");
+        User user2 = new User(2L,"ccc2",25,"ccc2@bk");
+        User user3 = new User(3L,"ccc3",26,"ccc3@bk");
+        User user4 = new User(4L,"ccc4",26,"ccc4@bk");
+
+        List<User> userList = new LinkedList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+
+
+        Map<Integer,List<User>> users = userList
+                .stream()
+                .parallel()
+                .collect(Collectors.groupingBy(User::getAge));
+
+
+
+
+
     }
 }
